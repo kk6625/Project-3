a67fd13dd3635b0f06a035eb8848223403dfaa8c
Kayla Flint <bookwm2016@gmail.com>
2024-09-16 18:50:00 -0400

    added userRoutes, routes to server.js, connection.js, and fixed .env file, database still won't connect though, so have to figure that out next

---
+++ client/server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
